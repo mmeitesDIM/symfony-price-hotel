284e3b43201670daf2269cf3a107049fcc566cb0
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 14:47:22 +0100

    Add Message Entity without getters/setters and Repository

---
+++ src/Entity/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MessageRepository")
+ */
+class Message {
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", name="message_id")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="messages")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="user_id")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Owner", inversedBy="messages")
+     * @ORM\JoinColumn(name="owner_id", referencedColumnName="owner_id")
+     */
+    private $owner;
+
+}
