92b3a4a30f74d038aa100d9e7486a0a1e14457d6
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 15:30:39 +0100

    Add Image Entity without getters/setters and Repository

---
+++ src/Entity/Image.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ImageRepository")
+ */
+class Image {
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="blob")
+     */
+    private $blob;
+
+}
