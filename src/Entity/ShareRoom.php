a93f17e26b6a374b01adefe98e19296f6718259b
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 22:27:57 +0100

    Update ShareRoom Entity with getters/setters

--- src/Entity/ShareRoom.php
+++ src/Entity/ShareRoom.php
@@ -10,8 +10,30 @@ use Doctrine\ORM\Mapping as ORM;
 class ShareRoom extends Property {
 
     /**
+     * Number of place leaving
+     *
      * @ORM\Column(type="integer")
      */
     private $placeLeaving;
+
+    /**
+     * Get placeLeaving
+     *
+     * @return int
+     */
+    public function getPlaceLeaving(): int {
+        return $this->placeLeaving;
+    }
+
+    /**
+     * Set placeLeaving
+     *
+     * @param int $placeLeaving
+     * @return ShareRoom
+     */
+    public function setPlaceLeaving(int $placeLeaving): self {
+        $this->placeLeaving = $placeLeaving;
+        return $this;
+    }
 
 }
