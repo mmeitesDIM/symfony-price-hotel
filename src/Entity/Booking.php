0a7fff7555e1186bade43e56bf949e2e4e4980dc
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 23:00:05 +0100

    Update Booking Entity with getters/setters

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -58,4 +58,163 @@ class Booking {
         $this->opinions = new ArrayCollection();
     }
 
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser(): User {
+        return $this->user;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     * @return Booking
+     */
+    public function setUser(User $user): self {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * Get totalPrice
+     *
+     * @return int
+     */
+    public function getTotalPrice(): int {
+        return $this->totalPrice;
+    }
+
+    /**
+     * Set totalPrice
+     *
+     * @param int $totalPrice
+     * @return Booking
+     */
+    public function setTotalPrice(int $totalPrice): self {
+        $this->totalPrice = $totalPrice;
+        return $this;
+    }
+
+    /**
+     * Get offer
+     *
+     * @return Offer
+     */
+    public function getOffer(): Offer {
+        return $this->offer;
+    }
+
+    /**
+     * Set offer
+     *
+     * @param Offer $offer
+     * @return Booking
+     */
+    public function setOffer(Offer $offer): self {
+        $this->offer = $offer;
+        return $this;
+    }
+
+    /**
+     * Get startDate
+     *
+     * @return \DateTime
+     */
+    public function getStartDate(): \DateTime {
+        return $this->startDate;
+    }
+
+    /**
+     * Set startDate
+     *
+     * @param \DateTime $startDate
+     * @return Booking
+     */
+    public function setStartDate(\DateTime $startDate): self {
+        $this->startDate = $startDate;
+        return $this;
+    }
+
+    /**
+     * Get endDate
+     *
+     * @return \DateTime
+     */
+    public function getEndDate(): \DateTime {
+        return $this->endDate;
+    }
+
+    /**
+     * Set endDate
+     *
+     * @param \DateTime $endDate
+     * @return Booking
+     */
+    public function setEndDate(\DateTime $endDate): self {
+        $this->endDate = $endDate;
+        return $this;
+    }
+
+    /**
+     * Get payed
+     *
+     * @return bool
+     */
+    public function getPayed(): bool {
+        return $this->payed;
+    }
+
+    /**
+     * Set payed
+     * @param bool $payed
+     * @return Booking
+     */
+    public function setPayed(bool $payed): self {
+        $this->payed = $payed;
+        return $this;
+    }
+
+    /**
+     * Get opinions
+     *
+     * @return ArrayCollection
+     */
+    public function getOpinions(): ArrayCollection {
+        return $this->opinions;
+    }
+
+    /**
+     * Set opinions
+     *
+     * @param ArrayCollection $opinions
+     * @return Booking
+     */
+    public function setOpinions(ArrayCollection $opinions): self {
+        $this->opinions = $opinions;
+        return $this;
+    }
+
+    /**
+     * Add opinion to opinions
+     *
+     * @param Opinion $opinion
+     * @return Booking
+     */
+    public function addOpinion(Opinion $opinion): self {
+        $this->opinions->add($opinion);
+        return $this;
+    }
+
 }
