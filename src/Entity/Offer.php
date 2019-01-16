53b8722a428410dd6e9bd204b75cf0515645bab7
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 14:49:27 +0100

    Add Offer Entity without getters/setters and Repository

---
+++ src/Entity/Offer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\OfferRepository")
+ */
+class Offer {
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", name="offer_id")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Property", inversedBy="offers")
+     * @ORM\JoinColumn(name="property_id", referencedColumnName="property_id")
+     */
+    private $property;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Alert", mappedBy="offer")
+     */
+    private $alerts;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Booking", mappedBy="offer")
+     */
+    private $bookings;
+
+    public function __construct() {
+        $this->alerts = new ArrayCollection();
+        $this->bookings = new ArrayCollection();
+    }
+
+}
