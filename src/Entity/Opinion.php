74d391352e8b2f825262d7abefaeef4495d40bf5
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 23:14:36 +0100

    Update Opinion Entity with getters/setters

--- src/Entity/Opinion.php
+++ src/Entity/Opinion.php
@@ -30,6 +30,75 @@ class Opinion {
      * @ORM\ManyToOne(targetEntity="Booking", inversedBy="opinions")
      * @ORM\JoinColumn(name="booking_id", referencedColumnName="booking_id")
      */
-    private $product;
+    private $booking;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage(): string {
+        return $this->message;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Opinion
+     */
+    public function setMessage(string $message): self {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * Get rated
+     *
+     * @return float
+     */
+    public function getRated(): float {
+        return $this->rated;
+    }
+
+    /**
+     * Set rated
+     *
+     * @param float $rated
+     * @return Opinion
+     */
+    public function setRated(float $rated): self {
+        $this->rated = $rated;
+        return $this;
+    }
+
+    /**
+     * Get booking
+     *
+     * @return Booking
+     */
+    public function getBooking(): Booking {
+        return $this->booking;
+    }
+
+    /**
+     * Set booking
+     *
+     * @param Booking $booking
+     * @return Opinion
+     */
+    public function setBooking(Booking $booking): self {
+        $this->booking = $booking;
+        return $this;
+    }
 
 }
