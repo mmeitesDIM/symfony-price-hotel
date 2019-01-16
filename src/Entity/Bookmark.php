684e781f8cf86fcc556d1d296a741507ccfc3fa5
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 22:49:02 +0100

    Update Bookmark Entity with getters/setters

--- src/Entity/Bookmark.php
+++ src/Entity/Bookmark.php
@@ -28,4 +28,52 @@ class Bookmark {
      */
     private $property;
 
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser(): User {
+        return $this->user;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     * @return Bookmark
+     */
+    public function setUser(User $user): self {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * Get property
+     *
+     * @return Property
+     */
+    public function getProperty(): Property {
+        return $this->property;
+    }
+
+    /**
+     * Set property
+     * @param Property $property
+     * @return Room
+     */
+    public function setProperty(Property $property): self {
+        $this->property = $property;
+        return $this;
+    }
+
 }
