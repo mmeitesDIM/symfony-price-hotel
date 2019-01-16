0a2468c8702ac8583f91ce36674d59c509330790
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 22:29:49 +0100

    Update FullHouse Entity with getters/setters

--- src/Entity/FullHouse.php
+++ src/Entity/FullHouse.php
@@ -10,7 +10,30 @@ use Doctrine\ORM\Mapping as ORM;
 class FullHouse extends Property {
 
     /**
+     * Number of room
+     *
      * @ORM\Column(type="integer")
      */
     private $roomNumber;
+
+    /**
+     * Get roomNumber
+     *
+     * @return int
+     */
+    public function getRoomNumber(): int {
+        return $this->roomNumber;
+    }
+
+    /**
+     * Set roomNumber
+     *
+     * @param int $roomNumber
+     * @return FullHouse
+     */
+    public function setRoomNumber(int $roomNumber): self {
+        $this->roomNumber = $roomNumber;
+        return $this;
+    }
+
 }
