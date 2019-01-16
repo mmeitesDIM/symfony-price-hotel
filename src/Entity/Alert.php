371356e803cb83c9c38ab0a846d19e4b6d226b00
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 14:41:02 +0100

    Add Alert Entity without getters/setters and Repository

---
+++ src/Entity/Alert.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AlertRepository")
+ */
+class Alert {
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", name="alert_id")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="User", inversedBy="alert")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="user_id")
+     */
+    private $user;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Offer", inversedBy="alert")
+     * @ORM\JoinColumn(name="offer_id", referencedColumnName="offer_id")
+     */
+    private $offer;
+
+}
