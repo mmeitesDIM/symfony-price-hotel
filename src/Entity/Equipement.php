b3f40e6d2ec147ead6a75351e99ee0c7513db7c2
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-16 23:09:38 +0100

    Update Equipement Entity with getters/setters

--- src/Entity/Equipement.php
+++ src/Entity/Equipement.php
@@ -42,4 +42,112 @@ class Equipement {
      */
     private $property;
 
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Equipement
+     */
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Equipement
+     */
+    public function setDescription(string $description): self {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get equipementType
+     *
+     * @return string
+     */
+    public function getEquipementType(): string {
+        return $this->equipementType;
+    }
+
+    /**
+     * Set equipementType
+     *
+     * @param string $equipementType
+     * @return Equipement
+     */
+    public function setEquipementType(string $equipementType): self {
+        $this->equipementType = $equipementType;
+        return $this;
+    }
+
+    /**
+     * Get imagePath
+     *
+     * @return string
+     */
+    public function getImagePath(): string {
+        return $this->imagePath;
+    }
+
+    /**
+     * Set imagePath
+     *
+     * @param string $imagePath
+     * @return Equipement
+     */
+    public function setImagePath(string $imagePath): self {
+        $this->imagePath = $imagePath;
+        return $this;
+    }
+
+    /**
+     * Get property
+     *
+     * @return Property
+     */
+    public function getProperty(): Property {
+        return $this->property;
+    }
+
+    /**
+     * Set property
+     * @param Property $property
+     * @return Equipement
+     */
+    public function setProperty(Property $property): self {
+        $this->property = $property;
+        return $this;
+    }
+
 }
