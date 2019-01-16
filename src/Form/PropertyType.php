3c52c0e23a308b1e250800893255e5b87b15efa0
panpan <patrick.lamatiere@gmail.com>
2019-01-16 13:15:37 +0100

    Add form
    Add  a form for create a new property in DB

---
+++ src/Form/PropertyType.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: digital
+ * Date: 16/01/2019
+ * Time: 10:11
+ */
+
+namespace App\Form;
+
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('address', TextType::class)
+            ->add('postalCode', TextType::class)
+            ->add('city', TextType::class)
+            ->add('country', TextType::class)
+            ->add('name', TextType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+        ]);
+    }
+}
