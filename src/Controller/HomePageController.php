a16e5422b193ea8e87b950020e17432663c34820
panpan <patrick.lamatiere@gmail.com>
2019-01-17 16:32:38 +0100

    add Front (navBar, card, Bootstrap)

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\FullHouseRepository;
+use App\Repository\PrivateRoomRepository;
+use App\Repository\ShareRoomRepository;
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
     /**
-     * @Route("/", name="home_page")
+     * @Route("/", name="home_page",  methods={"GET"})
      */
     public function index()
     {
+
+//        $fullHouserepo = new FullHouseRepository();
+
+        $response = $this->forward('App\Controller\FullHouseController::index', [/*$fullHouserepo*/]);
+        var_dump($response);
+
+
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
         ]);
