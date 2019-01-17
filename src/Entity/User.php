bc29420daf5073413065900a70c17b59048839a9
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-17 10:16:02 +0100

    Update User Entity with (email, password, roles) attributes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,12 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User {
+class User implements UserInterface {
 
     /**
      * @ORM\Id()
@@ -18,14 +19,25 @@ class User {
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
-    private $name;
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $mail;
+    private $name;
 
     /**
      * @ORM\OneToOne(targetEntity="Image")
@@ -64,13 +76,57 @@ class User {
         $this->messages = new ArrayCollection();
     }
 
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId(): int {
+    public function getId(): ?int {
         return $this->id;
+    }
+
+    public function getEmail(): ?string {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string {
+        return (string)$this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string {
+        return (string)$this->password;
+    }
+
+    public function setPassword(string $password): self {
+        $this->password = $password;
+
+        return $this;
     }
 
     /**
@@ -152,26 +208,6 @@ class User {
      */
     public function addMessage(Message $message): self {
         $this->messages->add($message);
-        return $this;
-    }
-
-    /**
-     * Get mail
-     *
-     * @return string
-     */
-    public function getMail(): string {
-        return $this->mail;
-    }
-
-    /**
-     * Set mail
-     *
-     * @param string $mail
-     * @return User
-     */
-    public function setMail(string $mail): self {
-        $this->mail = $mail;
         return $this;
     }
 
@@ -266,4 +302,19 @@ class User {
         return $this;
     }
 
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt() {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials() {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 }
