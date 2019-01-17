1556250e2070461e7b425b87513e289d6c7b4bad
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-17 22:07:40 +0100

    Clean home page display

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,29 +2,18 @@
 
 namespace App\Controller;
 
-use App\Repository\FullHouseRepository;
-use App\Repository\PrivateRoomRepository;
-use App\Repository\ShareRoomRepository;
-use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController extends AbstractController
-{
+class HomePageController extends AbstractController {
+
     /**
      * @Route("/", name="home_page",  methods={"GET"})
      */
-    public function index()
-    {
-
-//        $fullHouserepo = new FullHouseRepository();
-
-        $response = $this->forward('App\Controller\FullHouseController::index', [/*$fullHouserepo*/]);
-        var_dump($response);
-
-
-        return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
-        ]);
+    public function index() {
+        return $this->render(
+            'home_page/index.html.twig',
+            ['controller_name' => 'HomePageController',]
+        );
     }
 }
