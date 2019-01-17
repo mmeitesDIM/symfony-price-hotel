0efb3da54321770066fecb97bcd81d503cae465d
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2019-01-17 13:29:13 +0100

    Rename and remove unnecessary routes

--- src/Controller/FullHouseController.php
+++ src/Controller/FullHouseController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/full/house")
+ * @Route("/fullhouse")
  */
 class FullHouseController extends AbstractController
 {
